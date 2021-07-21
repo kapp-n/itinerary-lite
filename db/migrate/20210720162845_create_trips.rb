5abda632c733639cb7db1bfccadc98a2d6bb23ec
Mollie Dickenson <molliedickenson@gmail.com>
2021-07-21 16:08:50 -0400

    Added users, trips, categories, sessions and migrations

---
+++ db/migrate/20210720162845_create_trips.rb
@@ -0,0 +1,13 @@
+class CreateTrips < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trips do |t|
+      t.string :location
+      t.text :things_to_see
+      t.string :lodging
+      t.integer :category_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
